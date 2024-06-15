6969d9dd5988603224559b574e0050b560c691c8
Immanuel Kannan <immanuel@lendela.com>
2024-06-15 16:58:42 +0800

    Add oil nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -52,6 +52,34 @@ return {
     end,
   },
 
+  {
+    'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('nvim-web-devicons').setup {}
+    end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('oil').setup {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+        },
+        win_options = {
+          wrap = true,
+        },
+      }
+    end,
+  },
+
   -- {
   --   'dense-analysis/ale',
   --   config = function()
