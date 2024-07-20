ac7fe45759d415e8d746b9b4b78ada4856269514
Immanuel Kannan <immanuel@lendela.com>
2024-07-20 17:21:15 +0800

    Open mini files in directory of current buffer

--- lua/custom/plugins/mini-files.lua
+++ lua/custom/plugins/mini-files.lua
@@ -6,7 +6,7 @@ return {
       require('mini.files').setup();
 
       vim.keymap.set('n', '<leader>b', function()
-        if not MiniFiles.close() then MiniFiles.open() end
+        if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
       end, { desc = 'Toggle File [B]rowser' })
     end
   },
