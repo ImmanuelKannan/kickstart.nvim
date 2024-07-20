8c8d39e1b6abf6becf347fce17a285ba900ef61f
Immanuel Kannan <immanuel@lendela.com>
2024-07-20 17:09:27 +0800

    Standardize keymaps for vim-test

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -3,10 +3,10 @@ return {
   dependencies = {
     "preservim/vimux",
   },
-  vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
-  vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
-  vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
-  vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
+  vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest File' }),
+  vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est current [F]ile' }),
+  vim.keymap.set('n', '<leader>ta', ':TestSuite<CR>', { desc = '[T]est [A]ll Suite' }),
+  vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' }),
+  -- vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
   vim.cmd("let test#strategy = 'vimux'"),
 }
