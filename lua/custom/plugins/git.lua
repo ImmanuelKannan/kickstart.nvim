a357b210a8118f9a48bf6b5c3c8c1d430811266d
Immanuel Kannan <immanuel@lendela.com>
2025-04-05 14:54:02 +0800

    Add neogit diffview config

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,15 +1,31 @@
 return {
-  'NeogitOrg/neogit',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'sindrets/diffview.nvim',
-    'nvim-telescope/telescope.nvim',
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      local neogit = require 'neogit'
+      local diffview = require 'diffview'
+
+      diffview.setup {
+        view = {
+          merge_tool = {
+            layout = 'diff3_mixed',
+          },
+        },
+      }
+
+      neogit.setup {}
+
+      vim.keymap.set('n', '<leader>gs', neogit.open, { desc = '[G]it [S]tatus' })
+    end,
   },
-  config = function()
-    local neogit = require 'neogit'
-
-    neogit.setup {}
-
-    vim.keymap.set('n', '<leader>gs', neogit.open, { desc = '[G]it [S]tatus' })
-  end,
+  {
+    'akinsho/git-conflict.nvim',
+    version = '*',
+    config = true,
+  },
 }
