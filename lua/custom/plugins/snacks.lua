5d21a89a5b61aa5c033c83c6bfcaeee67fceb3cb
Immanuel Kannan <immanuel@lendela.com>
2025-04-09 19:09:46 +0800

    Replace telescope with snacks picker

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -3,6 +3,44 @@ return {
     'folke/snacks.nvim',
     opts = {
       scroll = {},
+      picker = {},
+    },
+    keys = {
+      {
+        '<leader><space>',
+        function()
+          Snacks.picker.files({ hidden = true, ignored = true, })
+        end,
+        desc = 'Find Files',
+      },
+      {
+        '<leader>sb',
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = 'Search Buffers',
+      },
+      {
+        '<leader>,',
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = 'Search Buffers',
+      },
+      {
+        '<leader>sk',
+        function()
+          Snacks.picker.keymaps()
+        end,
+        desc = 'Search Keymaps',
+      },
+      {
+        '<leader>sg',
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = 'Search Grep',
+      },
     },
   },
 }
