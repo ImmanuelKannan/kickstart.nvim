a92e91d368433e500b1ab8ab29962f85adb95405
Immanuel Kannan <immanuel@lendela.com>
2025-04-28 11:21:18 +0800

    Remove neogit in favor of lazygit

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,29 +1,5 @@
 return {
   {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'sindrets/diffview.nvim',
-      'nvim-telescope/telescope.nvim',
-    },
-    config = function()
-      local neogit = require 'neogit'
-      local diffview = require 'diffview'
-
-      diffview.setup {
-        view = {
-          merge_tool = {
-            layout = 'diff3_mixed',
-          },
-        },
-      }
-
-      neogit.setup {}
-
-      vim.keymap.set('n', '<leader>gs', neogit.open, { desc = '[G]it [S]tatus' })
-    end,
-  },
-  {
     'akinsho/git-conflict.nvim',
     version = '*',
     config = true,
