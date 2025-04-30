233ae525aceeaa3d484715a95a3baaee0f9e88b2
Immanuel Kannan <immanuel@lendela.com>
2025-04-30 13:53:00 +0800

    Re-add sindrets/diffview

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,5 +1,15 @@
 return {
   {
+    'sindrets/diffview.nvim',
+    opts = {
+      view = {
+        merge_tool = {
+          layout = 'diff3_mixed',
+        },
+      },
+    },
+  },
+  {
     'akinsho/git-conflict.nvim',
     version = '*',
     config = true,
