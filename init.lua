ce117a17c2f90cb94043a17feff3bb5e7d9b695f
Immanuel Kannan <immanueldk92@gmail.com>
2025-05-21 18:31:18 +0800

    Add zz keymapping to end of C-d and C-u

--- init.lua
+++ init.lua
@@ -177,6 +177,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 
