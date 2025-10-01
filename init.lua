282d26faa76875e1d43930e0d8ee6b7b2be3ec94
Immanuel <immanuel@lendela.com>
2025-10-01 14:13:38 +0800

    Replace ts_ls with vtsls and installed vue-language-server

--- init.lua
+++ init.lua
@@ -756,7 +756,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- ts_ls = {},
         eslint = {},
         vue_ls = {},
         vtsls = {
@@ -810,12 +810,57 @@ require('lazy').setup({
         filetypes = tsserver_filetypes,
       }
 
-      local vue_ls_config = {}
+      local vue_ls_config = {
+        cmd = { 'vue-language-server', '--stdio' },
+        filetypes = { 'vue' },
+        root_markers = { 'package.json' },
+        on_init = function(client)
+          local retries = 0
+
+          ---@param _ lsp.ResponseError
+          ---@param result any
+          ---@param context lsp.HandlerContext
+          local function typescriptHandler(_, result, context)
+            local ts_client = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'ts_ls' })[1]
+              or vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })[1]
+              or vim.lsp.get_clients({ bufnr = context.bufnr, name = 'typescript-tools' })[1]
+
+            if not ts_client then
+              -- there can sometimes be a short delay until `ts_ls`/`vtsls` are attached so we retry for a few times until it is ready
+              if retries <= 10 then
+                retries = retries + 1
+                vim.defer_fn(function()
+                  typescriptHandler(_, result, context)
+                end, 100)
+              else
+                vim.notify('Could not find `ts_ls`, `vtsls`, or `typescript-tools` lsp client required by `vue_ls`.', vim.log.levels.ERROR)
+              end
+              return
+            end
+
+            local param = unpack(result)
+            local id, command, payload = unpack(param)
+            ts_client:exec_cmd({
+              title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+              command = 'typescript.tsserverRequest',
+              arguments = {
+                command,
+                payload,
+              },
+            }, { bufnr = context.bufnr }, function(_, r)
+              local response_data = { { id, r and r.body } }
+              ---@diagnostic disable-next-line: param-type-mismatch
+              client:notify('tsserver/response', response_data)
+            end)
+          end
+
+          client.handlers['tsserver/request'] = typescriptHandler
+        end,
+      }
 
       vim.lsp.config('vtsls', vtsls_config)
       vim.lsp.config('vue_ls', vue_ls_config)
-      vim.lsp.config('ts_ls', ts_ls_config)
-      vim.lsp.enable { 'ts_ls', 'vue_ls' } -- If using `ts_ls` replace `vtsls` to `ts_ls`
+      vim.lsp.enable { 'vtsls', 'vue_ls' } -- If using `ts_ls` replace `vtsls` to `ts_ls`
       -- Ensure the servers and tools above are installed
       --
       -- To check the current status of installed tools and/or manually install
